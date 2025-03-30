26badfccc5eb0e97becafc765c1d93104c7f98e8
ikram-salahudeen <159075313+ikram-salahudeen@users.noreply.github.com>
2025-03-30 14:39:09 +0100

    FINAL

--- web/script.js
+++
@@ -1,6 +0,0 @@
-var socket = io.connect('http://' + window.location.hostname + ':' + location.port);
-
-        // Listen for 'update_counter' events from the server
-        socket.on('update_counter', function(data) {
-            document.querySelector('.counter').textContent = data.value;
-        });
