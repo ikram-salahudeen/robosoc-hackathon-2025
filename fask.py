4c256bbc2799789aa18af964e3a7d9c5d356384b
ikram-salahudeen <159075313+ikram-salahudeen@users.noreply.github.com>
2025-03-30 07:12:59 +0100

    Websocket fix

--- fask.py
+++ fask.py
@@ -1,69 +1,46 @@
-<<<<<<< HEAD
-from flask import Flask, render_template, request  
-from flask_socketio import SocketIO, emit
-import time
-import threading
-=======
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_socketio import SocketIO, emit
 
-import distance
->>>>>>> 2ba300acf2914ea429f92d82d918209a62bb93b2
 
 app = Flask(__name__, template_folder="web")
 socketio = SocketIO(app, async_handlers=True)  # Enable WebSocket support
 
-counter = 0
-
-def increment_counter():
-    global counter
-    while True:
-        socketio.sleep(1)  # Non-blocking sleep that yields control back to the event loop
-        counter += 1
-        socketio.emit('update_counter', {'value': counter})
-
-@socketio.on("my_event")
-def poll():
-    for x in range(5):
-        distance_data = distance.get_distances()
-
-        emit('server', {"distance1":1, "distance2":2}, room=sid)
-        socketio.sleep(1)
 
 @app.route('/')
 def home():
    return render_template('index.html')
-   
+
+# Friendly connect message
 @socketio.on('connect')
 def handle_connect():
-    """Send the current counter value when the client connects."""
-    socketio.emit('update_counter', {'value': counter})
+    socketio.emit('message', {'value': 'Connected'})
 
+# The html page sends this periodically to get some sensor data
+@socketio.on("request_data")
+def poll():
+    # Gather data
+
+    # Send it
+    emit('data', {'test_number':842, 'duty':0.5, 'dir':'Left', 'distance': 3.333, 'slouch': 1.2})
+
+# The HTML form sends this (works)
 @app.route('/control', methods=['POST'])
 def control():
-    global counter
     command = request.form.get('command')
     
     if command == "left":
-        counter -= 1  # Decrement the counter
-        print("left", counter)
-        socketio.emit('update_counter', {'value': counter})  # Emit updated counter
+        print("left")
+        socketio.emit('message', {'value': 'Recieved left'})
+
     elif command == "right":
-        counter += 1  # Increment the counter
-        print("right", counter)
-        socketio.emit('update_counter', {'value': counter})  # Emit updated counter
+        print("right")
+        socketio.emit('message', {'value': 'Recieved right'})
+
     elif command == "stop":
         print("stop")
+        socketio.emit('message', {'value': 'Recieved stop'})
     
     return "", 204  # No content response, just to acknowledge the request
-
-"""
-def update_text():
-    global dynamic_text
-    while True:
-        new_text = input("Enter new text for the webpage: ")
-        dynamic_text = new_text
-"""
 
 if __name__ == "__main__":
     #app.run(host="0.0.0.0", port=8500)
